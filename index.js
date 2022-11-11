d5022af22175f02b78dd757c87446cecfc17c7b1
zubayer <zubayer3570@gmail.com>
2022-11-11 12:00:34 +0600

    done

--- index.js
+++ index.js
@@ -92,11 +92,11 @@ const run = () => {
             res.send(result)
         })
         app.get('/get-ave-meal-rate', async (req, res) => {
-            const cursor1 = expenseCollection.find({})
+            const cursor1 = expenseCollection.find({}).project({date: 0, description: 0})
             const data1 = await cursor1.toArray()
             let totalAmount = 0;
             data1.forEach(singleExpense => totalAmount += parseInt(singleExpense.amount))
-
+            
             const cursor2 = personCollection.find({})
             const data2 = await cursor2.toArray()
             let totalMeal = 0;
